4a3612be5ac75f9bcbecca98a910c82064923605
Rodrigo Andrade <rodrigo.andrade@deway.com.br>
2017-12-14 11:20:39 -0300

    Header.js created and index.js updated

--- albums/index.js
+++ albums/index.js
@@ -1,4 +1,11 @@
+// Import a library to help create a component
+import React from 'react';
 import { AppRegistry } from 'react-native';
-import App from './App';
+import Header from './src/components/header';
+// Create a component
+const App = () => (
+    <Header headerText={'Albums'} />
+);
 
+//  Render it to the device
 AppRegistry.registerComponent('albums', () => App);
