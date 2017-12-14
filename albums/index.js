8321bb469115a3c5385a7f94f71b6b28cc75da8a
Rodrigo Andrade <rodrigo.andrade@deway.com.br>
2017-12-14 09:56:05 -0300

    Albums base project created

--- albums/index.js
+++ albums/index.js
@@ -1,10 +1,4 @@
-// Import a library to help create a component
-import React from "react";
-import { AppRegistry } from "react-native";
-import Header from ".src/components/header.js";
+import { AppRegistry } from 'react-native';
+import App from './App';
 
-// Create a component
-const App = () => <Header />;
-
-// Render it to the device
-AppRegistry.registerComponent("albums", () => App);
+AppRegistry.registerComponent('albums', () => App);
