faf4e2a460c95518494d57ffafbd3ac0defeac35
rodrigoftw <rodrigoftw2@gmail.com>
2017-12-18 08:53:36 -0300

    Album list is now scrollable

--- albums/index.js
+++ albums/index.js
@@ -5,7 +5,7 @@ import Header from './src/components/Header';
 import AlbumList from './src/components/AlbumList';
 // Create a component
 const App = () => (
-  <View>
+  <View style={{ flex: 1 }}>
     <Header headerText={'Albums'} />
     <AlbumList />
   </View>
