f1ff1b1db28d1229d25dd2680b8da5009e2ccbcd
rodrigoftw <rodrigoftw2@gmail.com>
2017-12-18 14:05:00 -0300

    Login form prototype created

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { Header } from './components/common'
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyABSVjeyOAOndCvY19cmiotgrW9O7lXEbQ',
+      authDomain: 'auth-e29e8.firebaseapp.com',
+      databaseURL: 'https://auth-e29e8.firebaseio.com',
+      projectId: 'auth-e29e8',
+      storageBucket: 'auth-e29e8.appspot.com',
+      messagingSenderId: '230736343917'
+    });
+  }
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <Text>An App!</Text>
+        <LoginForm />
       </View>
     );
   }
